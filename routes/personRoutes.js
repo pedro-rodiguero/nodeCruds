f1b8ee514b85a178230355db7b3fa5d96e9155a9
pemiinem <pedro.m.rodiguero@gmail.com>
2022-02-02 14:52:27 -0300

    add delete

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -89,4 +89,26 @@ router.patch('/:id', async (req, res) => {
     }
 })
 
+// Delete - deletar dados
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id
+    
+    const person = await Person.findOne({ _id: id })
+
+    if(!person) {
+        res.status(422).json({message: 'O usuario nao foi encontrado!!'})
+        return
+    }
+
+    try {
+        
+        await Person.deleteOne({_id: id})
+
+        res.status(200).json({message: 'Usuario removido com sucesso!!'})
+
+    } catch (error) {
+        res.status(500).json({error: error})
+    }
+})
+
 module.exports = router
